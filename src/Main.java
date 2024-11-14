07be9b75c4cff54c74a0c724fab2bba439f2d74f
lab <lab@lab1-35.NUCFD.edu.local>
2024-11-14 11:22:23 +0500

    Bugs fixed in main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
 		greetUser();
+		System.out.println("Changes introduced");
+
 	}
 
 	public static void greetUser() {
