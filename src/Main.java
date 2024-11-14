b1fa08e9bf21b006b0a95e87468f002bc3542ba1
lab <lab@lab1-35.NUCFD.edu.local>
2024-11-14 11:27:48 +0500

    Added changes to main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the application!");
-// greetUser(); // Removed this line to introduce a bug
-	}
-
-	public static void greetUser() {
-		System.out.println("Hello, User!");
+		System.out.println("Main branch update.");
 	}
 }
